1f644221d633ac222ceb00e92fa06f7bbc09e071
franyol <f.valbuenao64@gmail.com>
2024-04-06 02:28:11 -0500

    Set full screen true

--- src/main.cpp
+++ src/main.cpp
@@ -352,11 +352,11 @@ class SimpleGame: public LE_GameState
 
         void on_enter () {
 
-            bulletFreq = 10;
+            bulletFreq = 30;
             bulletTime = 0;
             bulletCount = 1;
             bulletMax = 100;
-            score = 0;
+            score = 46;
 
             // Load assets and game objects
             
